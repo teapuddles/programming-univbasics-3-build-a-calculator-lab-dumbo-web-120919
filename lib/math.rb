88ed444179bbc965a446c9bc866abb167070eb73
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-25 18:41:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,18 +1,21 @@
 def addition(num1, num2)
-  sum = num1 + num2
-  return sum
+  sumadd = num1 + num2
+  return sumadd
 end
 
 def subtraction(num1, num2)
-
+  sumsub = num2 - num1
+  return sumsub
 end
 
 def division(num1, num2)
-
+  sumdiv = num2 / num1
+  return sumdiv
 end
 
 def multiplication(num1, num2)
-
+  sumpro = num1 * num2
+  return sumpro
 end
 
 def modulo(num1, num2)
