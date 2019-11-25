9fce316532aa3b98e033bf7829ecf68d65c7e862
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-25 18:49:08 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sumadd = num1 + num2
+  return sumadd
 end
 
 def subtraction(num1, num2)
-
+  sumsub = num1 - num2
+  return sumsub
 end
 
 def division(num1, num2)
-
+  sumdiv = num1 / num2
+  return sumdiv
 end
 
 def multiplication(num1, num2)
-
+  sumpro = num1 * num2
+  return sumpro
 end
 
 def modulo(num1, num2)
-
+  summodulo = num1 % num2
+  return summodulo
 end
 
 def square_root(num)
-
+  sumsqrt = Math.sqrt(num)
+  return sumsqrt
 end
